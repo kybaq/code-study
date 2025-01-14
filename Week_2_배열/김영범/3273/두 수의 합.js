98f91fded4c46ce14c14bf86b948a27a21a6866d
kybaq <flfsk12@gmail.com>
2025-01-14 23:56:23 +0900

    feat: 배열 문제 '두 수의 합' 풀이 추가

---
+++ Week_2_배열/김영범/3273/두 수의 합.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+const input = String(fs.readFileSync(filePath)).trim().split("\n");
+const x = Number(input[2]);
+
+const numberArr = input[1].split(" ").map(Number);
+
+let count = 0;
+
+const seenValue = [];
+
+for (num of numberArr) {
+  const complement = x - num;
+
+  seenValue.push(num);
+
+  if (seenValue.includes(complement)) count++;
+}
+
+console.log(count);
+
+// const numberStr = input[1].split(" "); // 수열의 원소들
+// const numberArr = []; // 수열을 담을 배열
+
+// for (number of numberStr) {
+//   numberArr.push(Number(number)); // 원소 추가
+// }
+
+// for (let i = 0; i < length; i++) {
+//   //   const temp = []; // 조건을 만족하는 쌍을 저장하기 위함
+//   //   for (let k = i + 1; k < length; k++) {
+//   //     if (numberArr[k] === x - numberArr[i]) {
+//   //       temp.push(numberArr[i], numberArr[k]);
+//   //       answerArr.push(temp);
+//   //       answerArr.push(numberArr[k]);
+//   //     }
+//   //   }
+// }
+
+// console.log(numberArr.length);
