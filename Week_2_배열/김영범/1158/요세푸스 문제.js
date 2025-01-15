fc274e53b4f07313973933c3b760e7109ce72284
kybaq <flfsk12@gmail.com>
2025-01-16 00:08:55 +0900

    feat: 문제 '요세푸스 문제' 풀이 추가

--- Week_2_배열/김영범/1158/요세푸스 문제.js
+++ Week_2_배열/김영범/1158/요세푸스 문제.js
@@ -2,24 +2,20 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
 const input = String(fs.readFileSync(filePath)).trim().split(" ");
-
-const length = input[0];
-const removeNumber = input[1];
-
 const circular = [];
 
-for (let i = 1; i <= length; i++) {
+for (let i = 1; i <= input[0]; i++) {
   circular.push(i);
 }
 
-const permutation = [];
+let permutation = [];
 
 let index = 0;
 
 while (circular.length !== 0) {
-  index = (index + removeNumber - 1) % circular.length;
+  index = (index + input[1] - 1) % circular.length;
   let temp = circular.splice(index, 1);
   permutation.push(temp);
 }
 
-console.log(permutation);
+console.log(`<${permutation.join(", ")}>`);
