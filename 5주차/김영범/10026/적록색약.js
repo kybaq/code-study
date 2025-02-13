6b6995a76213c0ecec2fa0ab13758e1ff613aacd
kybaq <flfsk12@gmail.com>
2025-02-13 11:38:21 +0900

    5주차 문제 '적록색약 ' 풀이 추가 (실패)

--- 5주차/김영범/10026/적록색약.js
+++ 5주차/김영범/10026/적록색약.js
@@ -5,27 +5,28 @@ const filePath = "./input.txt";
 
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = input[0];
+const N = Number(input.shift());
+
 const queue = [];
 const visited = Array.from({ length: N }, () => new Array(N).fill(false));
 
 const dx = [1, 0, -1, 0];
 const dy = [0, 1, 0, -1];
 
-let last = ; // 마지막에 확인한 색, 여러 개인 경우는?
+let current = null; // 현재 확인한 색
+let last = null; // 마지막에 확인한 색
 let area = 0; // 영역 수
 let area_cb = 0; // 적록색맹일 경우 영역 수
 
-for (let y = 1; y <= N; y++) {
-  for (let x = 1; x <= N; x++) {
-    const current = input[y][x]; // 현재 색깔
-    last = last ? last : current; // 직전에 확인한 색이 없으면, 현재 색으로 지정.
+for (let y = 0; y < N; y++) {
+  for (let x = 0; x < N; x++) {
+    current = input[y][x]; // 현재 색깔
 
-    if (current === last || visited[y][x]) continue; // 같은 색일 경우 or 이미 방문했으면 그냥 넘어감.
+    if (visited[y][x] || current === last) continue; // 같은 색일 경우 or 이미 방문했으면 그냥 넘어감.
     else {
-        // 직전에 방문하지 않았으면서, 직전에 확인한 값과 다른 경우우
+        // 직전에 방문하지 않았으면서, 직전에 확인한 값과 다른 경우
+      visited[y][x] = true;
       queue.push([x, y]);
-      // current =
     }
 
     while (queue.length) {
@@ -35,12 +36,29 @@ for (let y = 1; y <= N; y++) {
         const nx = curX + dx[i];
         const ny = curY + dy[i];
 
-        if (nx < 0 || nx > N || ny < 0 || ny > N) continue;
-        else if (current === last || visited[ny][nx]) continue;
+        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+        else if (visited[ny][nx] || current === last) continue;
         else {
-            // 범위 안의 값이면서, 방문하지도 않았고, 직전에 발견한 새로운 값과도 다른 경우우
+          console.log(ny, nx, current, last); 
+          // 범위 안의 값이면서, 방문하지도 않았고, 직전에 방문한 곳의 색과도 다른 경우
+          visited[ny][nx] = true; // 방문 여부 갱신
+          last = input[curY][curX]; // 직전에 방문한 값 갱신
+          current = input[ny][nx]; // 현재 값 갱신
+          queue.push([nx, ny]);
+
+          // 적록색맹일 경우 계산을 다르게 하기 위한 조건문
+          if (visited[curY][curX] || current === last) continue;
+          else {
+            if((current === "R" && last === "G") ||  (current === "G" && last === "R")) area++;
+            else {
+            area++;
+            area_cb++;
+            }
+          }
         }
       }
     }
   }
 }
+
+console.log(area, area_cb); 
