f039213f6800abca60dffc60d466916131dee04f
조으냐 <eunyaaaaa@jo-eunyaui-noteubug.local>
2025-02-21 23:56:01 +0900

    7주차 '시리얼 번호' 문제 풀이 수정

--- 7주차/조은영/[1431]시리얼번호.py
+++ 7주차/조은영/[1431]시리얼번호.py
@@ -8,7 +8,8 @@ def sum_of_digits(serial):
 N = int(sys.stdin.readline().strip())  # 기타 개수
 serials = [sys.stdin.readline().strip() for _ in range(N)]  # 시리얼 번호 리스트
 
-# 정렬 수행 (우선순위: 길이 -> 숫자 합 -> 사전순)
+# 정렬 수행
+# 우선순위 - 길이 -> 숫자 합 -> 사전
 serials.sort(key=lambda x: (len(x), sum_of_digits(x), x))
 
 for serial in serials:
